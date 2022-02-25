1344781aca587a580d8670cca9faffd669be8d0e
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-02-25 16:03:17 +0100

    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#keep-going-more-requirements works

--- hello.go
+++ hello.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPerefix = "Hola, "
+const frenchHelloPerefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+	if language == "Spanish" {
+		return spanishHelloPerefix + name
+	}
+	if language == "French" {
+		return frenchHelloPerefix + name
 	}
 	return englishHelloPrefix + name
 }
--- hello_test.go
+++ hello_test.go
@@ -2,10 +2,10 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
+func TestName(t *testing.T) {
 	assertCorrectMessage := func(t testing.TB, bring string, want string) {
 		t.Helper()
-		if got := Hello(bring); got != want {
+		if got := Hello(bring, ""); got != want {
 			t.Errorf("Hello() = %v, want %v", got, want)
 		}
 	}
@@ -23,6 +23,37 @@ func TestHello(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assertCorrectMessage(t, tt.bring, tt.want)
+		})
+	}
+}
+
+func TestLanguage(t *testing.T) {
+	type args struct {
+		name     string
+		language string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "in Spanish",
+			args: args{name: "Elodie", language: "Spanish"},
+			want: "Hola, Elodie",
+		},
+
+		{
+			name: "in French",
+			args: args{name: "Elodie", language: "French"},
+			want: "Bonjour, Elodie",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Hello(tt.args.name, tt.args.language); got != tt.want {
+				t.Errorf("Hello() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
