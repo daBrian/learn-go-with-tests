6c9e2a7e06e04dea0acd98f1858efa5dbd5553dc
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-02-27 23:26:26 +0100

    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#refactor introduce struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,14 @@
 package structs
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	width  float64
+	height float64
 }
 
-func Area(width float64, height float64) interface{} {
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+func Area(rectangle Rectangle) interface{} {
+	return rectangle.width * rectangle.height
 }
