30cd4ceeca81df121c6f4d92b76662998eb3369e
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-02-27 22:37:22 +0100

    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-enough-code-to-make-it-pass-3 okay, a little bit shorter

--- sum.go
+++ sum.go
@@ -14,3 +14,9 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers[1:]))
+	}
+	return
+}
