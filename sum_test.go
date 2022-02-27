75b14d641e525152786ed19956058998c1582364
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-02-27 22:56:34 +0100

    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-enough-code-to-make-it-pass-4

--- sum_test.go
+++ sum_test.go
@@ -25,12 +25,40 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
-
-func TestSumAllTails(t *testing.T) {
+func TestSumAllTailsa(t *testing.T) {
 	got := SumAllTails([]int{1, 2}, []int{0, 9})
 	want := []int{2, 9}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	type args struct {
+		numbersToSum [][]int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantSums []int
+	}{
+		{
+			"make the sums of some slices",
+			args{[][]int{[]int{1, 2}, []int{0, 9}}},
+			[]int{2, 9},
+		},
+		{
+			"safely sum empty slices",
+			args{[][]int{[]int{}, []int{3, 4, 5}}},
+			[]int{0, 9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSums := SumAllTails(tt.args.numbersToSum...); !reflect.DeepEqual(gotSums, tt.wantSums) {
+				t.Errorf("SumAllTails() = %v, want %v", gotSums, tt.wantSums)
+			}
+		})
 	}
 }
