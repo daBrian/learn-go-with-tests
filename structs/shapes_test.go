d5f4abab6ae0c192e822dacfc50c28965af344d3
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-01 11:28:49 +0100

    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#further-refactoring and add names

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -12,20 +12,21 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{"Rectangle", Rectangle{12.0, 6.0}, 72},
+		{"Circle", Circle{10}, 314.1592653589793},
+	}
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want %g", got, tt.want)
+			}
+		})
+	}
 }
