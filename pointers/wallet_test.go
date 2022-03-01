98ca15bb6fef90a5b6c7c318b1308c6206d2e5a4
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-01 22:32:40 +0100

    extracted errorcheck
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors#refactor

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,12 +13,10 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, want)
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraww with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(10))
-		if err != nil {
-			t.Errorf("received error %q", err)
-		}
+		assertNoError(t, err)
 		want := Bitcoin(10)
 		assertBalance(t, wallet, want)
 	})
@@ -38,6 +36,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	got := wallet.Balance()
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
