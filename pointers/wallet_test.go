1dafc3031df7ee2d2f3f2f192d2a4a4b46126f81
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-01 21:51:57 +0100

    refactor after withdraw
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -8,19 +8,21 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-		got := wallet.Balance()
 		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
-		got := wallet.Balance()
 		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
 }
