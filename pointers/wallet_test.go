fe45b4e8c08ba3fa5e68c70df7a136639c6d25d6
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-01 21:48:24 +0100

    withdraw
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,11 +5,22 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
