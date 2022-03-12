f85a2221326478918d43cef9618aeeceaf8ee620
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-12 22:20:22 +0100

    introduce testserver
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/select

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,15 +1,29 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
 
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+	slowServer.Close()
+	fastServer.Close()
 }
