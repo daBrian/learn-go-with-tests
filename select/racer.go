e417506a315abac16dae8d686ef8766105041a93
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-12 22:31:48 +0100

    select
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/select

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,21 @@
 package racer
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponse(a)
-
-	bDuration := measureResponse(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponse(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
