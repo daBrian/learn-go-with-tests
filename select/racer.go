cf6fb6a03f5a748c135e58db04d93aba2564a07a
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-12 22:26:47 +0100

    defer
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/select

--- select/racer.go
+++ select/racer.go
@@ -6,16 +6,18 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
+	aDuration := measureResponse(a)
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	bDuration := measureResponse(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 	return b
+}
+
+func measureResponse(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,22 +8,24 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(1 * time.Millisecond)
+	fastServer := makeDelayedServer(0)
+	defer slowServer.Close()
+	defer fastServer.Close()
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
+
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
-	slowServer.Close()
-	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
