2f7567bdcc38abcc252dd11f13a5f2553cebe62d
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-15 09:35:41 +0100

    refactor test
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reflection

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -1,8 +1,11 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestWalk(t *testing.T) {
+func TestWalk2(t *testing.T) {
 	expected := "Chris"
 	var got []string
 
@@ -19,5 +22,32 @@ func TestWalk(t *testing.T) {
 	}
 	if got[0] != expected {
 		t.Errorf("got %q, want %q", got[0], expected)
+	}
+}
+
+func Test_walk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			var got []string
+			walk(tt.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, tt.ExpectedCalls) {
+				t.Errorf("got %v want %v", got, tt.ExpectedCalls)
+			}
+		})
 	}
 }
