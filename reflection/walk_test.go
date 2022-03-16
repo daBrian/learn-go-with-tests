2e53a9270e9e6503ebf2908c9916aec5a0b1505f
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-16 21:37:58 +0100

    function
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reflection

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -109,6 +109,22 @@ func Test_walk(t *testing.T) {
 			t.Errorf("got %v, want %v", got, want)
 		}
 	})
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
 func assertContains(t testing.TB, haystack []string, needle string) {
 	t.Helper()
