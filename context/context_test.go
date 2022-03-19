450e57e668a7b6a74e919274ad3fd370edc5daa0
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-03-19 17:31:25 +0100

    context complete
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/context

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -39,10 +40,26 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	}
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestServer(t *testing.T) {
+	data := "hello, world"
 
 	t.Run("Happy path", func(t *testing.T) {
-		data := "hello, world"
 		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
@@ -57,22 +74,22 @@ func TestServer(t *testing.T) {
 
 	})
 
-	//t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-	//	data := "hello world"
-	//	store := &SpyStore{response: data, t: t}
-	//	srv := Server(store)
-	//
-	//	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	//
-	//	cancellingCtx, cancel := context.WithCancel(request.Context())
-	//	time.AfterFunc(5*time.Millisecond, cancel)
-	//	request = request.WithContext(cancellingCtx)
-	//
-	//	response := httptest.NewRecorder()
-	//
-	//	srv.ServeHTTP(response, request)
-	//
-	//	store.assertWasCancelled()
-	//})
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		srv := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := &SpyResponseWriter{}
+
+		srv.ServeHTTP(response, request)
+		if response.written {
+			t.Error("a response should not have been written")
+		}
+	})
 
 }
