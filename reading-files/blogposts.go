ceb7e90d8f830164c26ed34fdbcd743bc6fc57b6
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-07-15 10:23:34 +0200

    read files but ignore errors
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reading-files#write-enough-code-to-make-it-pass-1

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,17 +1,27 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
 type Post struct {
+	Title string
 }
 
-func NewPostsFromFs(fileSystem fs.FS) []Post {
+func NewPostsFromFs(fileSystem fs.FS) ([]Post, error) {
 	dir, _ := fs.ReadDir(fileSystem, ".")
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		posts = append(posts, getPost(fileSystem, f))
 	}
-	return posts
+	return posts, nil
+}
+
+func getPost(system fs.FS, f fs.DirEntry) Post {
+	postFile, _ := system.Open(f.Name())
+	defer postFile.Close()
+	postData, _ := io.ReadAll(postFile)
+	post := Post{Title: string(postData)[7:]}
+	return post
 }
--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -1,19 +1,37 @@
 package blogposts_test
 
 import (
+	"errors"
 	blogposts "github.com/daBrian/learn-go-with-tests/reading-files"
+	"github.com/stretchr/testify/assert"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("Goodbye, World!")
+}
+
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts := blogposts.NewPostsFromFs(fs)
+	posts, err := blogposts.NewPostsFromFs(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	if len(posts) != len(fs) {
 		t.Errorf("got %d want %d", len(posts), len(fs))
 	}
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	assert.Equal(t, want, got)
 }
