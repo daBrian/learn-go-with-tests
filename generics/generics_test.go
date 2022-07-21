fcb545165a4a79706f716acff647d028acdb44cd
Matthias Brian <2210577+daBrian@users.noreply.github.com>
2022-07-21 22:15:05 +0200

    Refactor
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/generics#a-function-that-takes-a-string-or-an-integer-or-indeed-other-things

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -1,29 +1,21 @@
 package generics_test
 
-import "testing"
+import (
+	. "github.com/daBrian/learn-go-with-tests/generics"
+	"testing"
+)
 
 func TestAssertFunctions(t *testing.T) {
 	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, 1, 1)
-		AssertNotEqual(t, 1, 2)
+		MAssertEqual(t, 1, 1)
+		MAssertNotEqual(t, 1, 2)
 	})
-}
-func TestAssertFunctionsWithStrings(t *testing.T) {
-	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, "mary", "mary")
-		AssertNotEqual(t, "mary", "john")
+	t.Run("asserting on strings", func(t *testing.T) {
+		MAssertEqual(t, "mary", "mary")
+		MAssertNotEqual(t, "mary", "john")
 	})
-}
-func AssertEqual(t *testing.T, got, want interface{}) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %+v, want %+v", got, want)
-	}
-}
 
-func AssertNotEqual(t *testing.T, got, want interface{}) {
-	t.Helper()
-	if got == want {
-		t.Errorf("didn't want %+v", got)
-	}
+	//t.Run("assert mixed", func(t *testing.T) {
+	//	MAssertEqual(t, 1, "1")
+	//})
 }
